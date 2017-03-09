fdc87b9217b7e9f56dcfc9d3190cf2222943500f
lenriquez <felipe096@gmail.com>
2017-03-09 14:42:58 -0500

    Update shared/index.ts

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,2 +1,4 @@
 export { DropdownDirective } from './dropdown.directive';
 export { ImageUrlPipe } from './image-url.pipe';
+export { TranslateGenrePipe } from './translate-genre.pipe';
+export { GenreService } from './genre.service';
